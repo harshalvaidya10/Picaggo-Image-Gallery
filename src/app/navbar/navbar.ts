4f3a73e4978eb0cf8972e4d7f17b8e2d3e6d9edc
Harshal Vaidya <harshalvaidya@Mac.lan>
2025-06-18 02:51:28 -0700

    made nav bar responsive

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.css'
 })
 export class Navbar {
-
+  menuOpen = false;
+  navLinks = [
+    { label: 'Home', href: '#' },
+    { label: 'Features', href: '#' },
+    { label: 'Screenshots', href: '#' },
+    { label: 'FAQs', href: '#' },
+    { label: 'Pricing', href: '#' },
+    { label: 'Contact', href: '#' }
+  ];
 }
